45d15ffc620c01f6f7a97d9275e6fb185ac36e21
mason125 <mason.white@snhu.edu>
2017-05-07 22:52:12 -0500

    Week1 665
    Added collapsing nave bar with links also changed the CDN link for
    Bootstraps

--- Week1_665/index.php
+++ Week1_665/index.php
@@ -7,11 +7,10 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-         <!-- Latest compiled and minified CSS -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <title></title>
     </head>
@@ -25,6 +24,17 @@ and open the template in the editor.
                         <span class ="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" style="color:red">txt</a>
+                </div>
+                <div class ="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class ="nav navbar-nav">
+                        <li><a href="#">Layout</a></li>
+                        <li><a href="#">Flotr2</a></li>
+                        <li><a href="#">Chart.js</a></li>
+                        <li><a href="#">Chartlist.js</a></li>
+                        <li><a href="#">Google Chart</a></li>
+                        <li><a href="#">D3</a></li>
+                        <li><a href="#">Leaflet</a></li>
+                    </ul>
                 </div>
             </nav>
         </div>
