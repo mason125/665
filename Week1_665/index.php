bc6b8c80eb0d813dd52e9bff085955f47970cf6a
mason125 <mason.white@snhu.edu>
2017-05-09 23:20:24 -0500

    Week1 665
    Project complete, arrays are installed and Boostraps is fucntinoning as
    it should be!

--- Week1_665/index.php
+++ Week1_665/index.php
@@ -12,11 +12,13 @@ and open the template in the editor.
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src ="functions.js"></script>
+        <script src ="functions.js" type="text/javascript"></script>
+        <link rel ="stylesheet" href="CSS.css" type ="text/css">
         <title>MSSE 665</title>
     </head>
     <body>
         <div id ="wrapper">
+            <!--nav bar-->
             <nav class ="navbar navbar-inverse navbar-fixed-top" role ="navigation">
                 <div class ="navbar-header">
                     <button type ="button" class ="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
@@ -26,9 +28,10 @@ and open the template in the editor.
                     </button>
                     <a class="navbar-brand" style="color:red">txt</a>
                 </div>
+                <!--nav bar links-->
                 <div class ="collapse navbar-collapse navbar-ex1-collapse">
                     <ul class ="nav navbar-nav">
-                        <li><a onclick="clearKPIs()">Layout</a></li>
+                        <li><a onclick="clearKpis()">Layout</a></li>
                         <li><a onclick="flort2()">Flotr2</a></li>
                         <li><a onclick="chartjs()">Chart.js</a></li>
                         <li><a onclick="chartist()">Chartist.js</a></li>
@@ -44,6 +47,7 @@ and open the template in the editor.
                     <div class ="row">
                         <div class ="col-lg-12">
                             <br>
+                            <!-- content for header-->
                             <h2 class="page-header" style="text-align: center">
                                 KPI
                             </h2>
@@ -51,20 +55,34 @@ and open the template in the editor.
                     </div>
                 </div>
                 <!--graphs KPI containers will respond to screen size-->
-                <div class="container-fluid bg-3 text-center">
+                <div class="container-fluid bg-3 text-center kpis">
                     <div class="row">
                         <div class ="col-sm-4">
                             <p>Graph 1</p>
+                            <div id ="K1"></div>
                         </div>
                         <div class ="col-sm-4">
                             <p>Graph 2</p>
+                            <div id ="K2"></div>
                         </div>
                         <div class="col-sm-4">
                             <p>Graph 3</p>
+                            <div id ="K3"></div>
                         </div>
                     </div>
                 </div>
+                <div class ="container-fluid bg-3 text-center kpis">
+                    <div class ="row">
+                        <p>Graph 4</p>
+                        <div id ="K4"></div>
+                    </div>
+                </div>
             </div>
-        </div>
+        <br>
+        <!--footer code-->
+        <footer class="container-fluid bg-4 text-center" style ="background-color:black; color:white;">
+            <p> Data visuals </p>
+            <p><a href="http://www.ackmeedevlab.com">www.ackmeedevlabs.com</a></p>
+        </footer>
     </body>
 </html>
