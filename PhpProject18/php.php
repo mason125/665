2e0f2d4e7614c4e3a9dece3d83c536e4c403adbe
mason125 <mason.white@snhu.edu>
2017-06-03 23:53:03 -0500

    Week 5
    php ajax call that works not chart ready.  JS codes for working chart
    commented out

---
+++ PhpProject18/php.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    //assign vars after formatting to avoid a wrong login with correct 
+    //username and password
+    echo json_encode("15");
+}
+
+
