f56c21d6b44c3f7794d6b942656ed1050aeb8f24
mason125 <mason.white@snhu.edu>
2017-06-08 22:38:04 -0500

    Week 5 MSSE 665 SQL Script
    Will create a DB, Table and fill the table

---
+++ MYSQL.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE msse6652;
+
+CREATE TABLE mason6(
+	val INT NOT NULL
+);
+
+INSERT INTO mason6 (`val`) VALUES (1);
+INSERT INTO mason6 (`val`) VALUES (11);
+INSERT INTO mason6 (`val`) VALUES (15);
+INSERT INTO mason6 (`val`) VALUES (17);
+INSERT INTO mason6 (`val`) VALUES (24);
+INSERT INTO mason6 (`val`) VALUES (20);
+INSERT INTO mason6 (`val`) VALUES (19);
+INSERT INTO mason6 (`val`) VALUES (10);
+INSERT INTO mason6 (`val`) VALUES (13);
+INSERT INTO mason6 (`val`) VALUES (2);
+INSERT INTO mason6 (`val`) VALUES (3);
+INSERT INTO mason6 (`val`) VALUES (7);
+INSERT INTO mason6 (`val`) VALUES (14);
+INSERT INTO mason6 (`val`) VALUES (16);
+INSERT INTO mason6 (`val`) VALUES (22);
+INSERT INTO mason6 (`val`) VALUES (6);
+INSERT INTO mason6 (`val`) VALUES (7);
+INSERT INTO mason6 (`val`) VALUES (9);
+INSERT INTO mason6 (`val`) VALUES (7);
+INSERT INTO mason6 (`val`) VALUES (2);
